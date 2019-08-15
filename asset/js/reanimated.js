3fd6f7a9051a8d1a7b34649f0473d2e1e8cc845d
David Banks <amoebae@gmail.com>
2019-08-15 13:39:36 +0100

    fixing masonry

--- asset/js/reanimated.js
+++ asset/js/reanimated.js
@@ -1,17 +1,27 @@
+function onLayoutComplete(e) {
+    console.log("masonry: layout complete");
+}
+
 function onReady(e) {
     console.log("document is ready");
 
     console.log("Masonry:", Masonry);
 
-    const gridElement = document.getElementById('the-grid');
+    // XXX: should be queryselectorall but didn't work very well creating
+    // multiple handles...
+    const gridElement = document.querySelector('.rdt-masonry-grid');
 
     // crams items into a layout
     const masonryOptions = {
         columnWidth: 400,
-        itemSelector: '.grid-item'
+        gutter: 0,
+        itemSelector: '.rdt-grid-item'
     };
 
     const masonry = new Masonry(gridElement, masonryOptions);
+    masonry.on('layoutComplete', onLayoutComplete);
+
+    masonry.layout();
 }
 
 document.addEventListener('DOMContentLoaded', onReady);
