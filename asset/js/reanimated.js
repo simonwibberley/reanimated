c5ffe55562ea61d01dbafdf1b8f1ad3ba34e47b3
David Banks <amoebae@gmail.com>
2019-08-15 12:51:02 +0100

    using masonry

--- asset/js/reanimated.js
+++ asset/js/reanimated.js
@@ -1,6 +1,17 @@
 function onReady(e) {
-    console.log("dom is ready");
+    console.log("document is ready");
+
+    console.log("Masonry:", Masonry);
+
+    const gridElement = document.getElementById('the-grid');
+
+    // crams items into a layout
+    const masonryOptions = {
+        columnWidth: 400,
+        itemSelector: '.grid-item'
+    };
+
+    const masonry = new Masonry(gridElement, masonryOptions);
 }
 
 document.addEventListener('DOMContentLoaded', onReady);
-
